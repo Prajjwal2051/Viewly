ae2d74c0ab40cdb3bb80c85a2315539acb4785d9
Prajjwal2051 <prajjwal2051@gmail.com>
2025-09-26 22:51:59 +0530

    added an async handler modified app.js and index,js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,15 @@ import connectDB from "./db/db_connection.js";
 dotenv.config({
     path:"./env"
 })
-connectDB()
+connectDB() // this returns a promise
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at PORT: ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("mongoDB connect failed !!!")
+})
 /*
 
 this is also the way of connecting to the database but we prefer writing this code in src/db/db_connection.js so that we maintian the professional approach
