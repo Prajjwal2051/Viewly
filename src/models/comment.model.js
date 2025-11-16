75378cc9b240485b1903bf4abbeaaa495443bdd3
Prajjwal2051 <prajjwal2051@gmail.com>
2025-11-16 17:04:15 +0530

    feat(models): add comment, dislike, and like schemas with aggregate pagination support

---
+++ src/models/comment.model.js
@@ -0,0 +1,40 @@
+import mongoose, { mongo, Schema, schema } from "mongoose"
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
+
+const commentSchema = new mongoose.Schema(
+    {
+        content: {
+            type: String,
+            required: true,
+            trim: true,
+            minlength: 1,
+            maxlength: 500,
+        },
+        video: {
+            type: Schema.Types.ObjectId,
+            ref: "video",
+            required: true,
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        parentComment: {
+            type: Schema.Types.ObjectId,
+            ref: "Comment",
+            default: null, // this is for the nested comment
+        },
+        likes: {
+            type: Number,
+            default: 0,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+commentSchema.plugin(mongooseAggregatePaginate)
+
+export const comment = mongoose.model("Comment", commentSchema)
---
+++ src/models/like.model.js
@@ -0,0 +1,28 @@
+import mongoose, { Mongoose,mongo, Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+import { comment } from "./comment.model";
+
+const likeSchema=new mongoose.Schema({
+    video:{
+        type:Schema.Types.ObjectId,
+        ref:'video',
+    },
+    comment:{
+        type:Schema.Types.ObjectId,
+        ref:'Comment'
+    },
+    likedBy:{
+        type:Schema.Types.ObjectId,
+        ref:'User',
+        required:true,
+    },
+},{
+    timestamps:true
+})
+likeSchema.plugin(mongooseAggregatePaginate)
+
+// and we also need to ensure that a particular user likes a video or comment only once
+likeSchema.index({video:1,likedBy:1},{unique:true,sparse:true})
+likeSchema.index({comment:1,likedBy:1},{unique:true,sparse:true})
+
+export const dislike=mongoose.model("Like",likeSchema)
